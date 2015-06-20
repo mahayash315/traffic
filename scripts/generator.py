183d32908c87bceb3aa2f6b52ca658b2b1b0dfbe
TopGear <tglover.nuppy@gmail.com>
2015-06-21 02:03:33 +0900

    some updates

---
+++ scripts/generator.py
@@ -0,0 +1,54 @@
+import math
+import random
+import numpy as np
+import plot as plt
+
+
+class Generator(object):
+    def __init__(self):
+        self.last_state = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        raise StopIteration
+
+class SimpleGenerator(Generator):
+    def __init__(self, num=1):
+        super(SimpleGenerator, self).__init__()
+        self.num = num
+        self.i = 0
+
+    def f(self, x):
+        return math.sin(x / math.pi)
+
+    def noise(self, x):
+        return (random.random() * 0.1)
+
+    def itrgenerate(self, x, l):
+        y = self.f(x)
+        y += self.noise(x)
+        return y
+
+    def generate(self, x):
+        arr = [0 for _ in xrange(self.num)]
+        for l in xrange(self.num):
+            arr[l] = self.itrgenerate(x, l)
+        return np.array(arr)
+
+    def next(self):
+        x = self.i
+        self.i += 1
+        return self.generate(x)
+
+
+if __name__ == '__main__':
+    try:
+        plotter = plt.Plotter()
+        gen = SimpleGenerator()
+        for y in gen:
+            print("{}".format(y))
+            plotter.append(y)
+    except Exception as e:
+        print(str(e))
--- scripts/plot.py
+++ scripts/plot.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 
 def plot(Y, pred_Y=None, title='', block=True):
@@ -24,12 +24,70 @@ def plot(Y, pred_Y=None, title='', block=True):
 	Yt = Y.transpose()
 	if pred_Y == None:
 		for i in xrange(m):
-			ax[i].plot(numpy.array(xrange(n)), Yt[i], "b.-")
+			ax[i].plot(np.array(xrange(n)), Yt[i], "b.-")
 	else:
 		pred_Yt = pred_Y.transpose()
 		for i in xrange(m):
-			ax[i].plot(numpy.array(xrange(n)), Yt[i], "b.-")
-			ax[i].plot(numpy.array(xrange(n)), pred_Yt[i], "r.-")
+			ax[i].plot(np.array(xrange(n)), Yt[i], "b.-")
+			ax[i].plot(np.array(xrange(n)), pred_Yt[i], "r.-")
 
 	plt.title(title)
 	plt.show(block=block)
+
+class Plotter:
+	def __init__(self):
+		self.fig = None
+		self.ax = None
+		self.plot_y = None
+		self.plot_pred_y = None
+		self.last_x = -1
+
+	def drawFrame(self):
+		if (self.fig == None):
+			self.fig = plt.figure()
+			plt.show(block=False)
+
+	def appendFirst(self, y, pred_y=None):
+		m = len(y)	# the number of observation locations
+
+		# create axes
+		ax = []
+		for i in xrange(m):
+			ax.append(self.fig.add_subplot(m,1,i+1))
+
+		# plot the data
+		plot_y = []
+		plot_pred_y = []
+		yt = y.transpose()
+		if pred_y == None:
+			for i in xrange(m):
+				plot_y.extend(ax[i].plot(np.array([0]), yt[i], "b.-"))
+		else:
+			pred_yt = pred_y.transpose()
+			for i in xrange(m):
+				plot_y.extend(ax[i].plot(np.array([0]), yt[i], "b.-"))
+				plot_pred_y.extend(ax[i].plot(np.array([0]), pred_yt[i], "r.-"))
+
+		self.ax = ax
+		self.plot_y = plot_y
+		self.plot_pred_y = plot_pred_y
+		self.last_x = 0
+
+	def append(self, y, pred_y=None):
+		if (self.fig == None):
+			self.drawFrame()
+
+		if (self.last_x == -1):
+			self.appendFirst(y, pred_y)
+		else:
+			x = self.last_x + 1
+			for i in xrange(len(self.ax)):
+				plot_y = self.plot_y[i]
+				# plot_pred_y = self.plot_pred_y[i] # FIXME: to be fixed later
+				plot_y.set_xdata(np.append(plot_y.get_xdata(), x))
+				plot_y.set_ydata(np.append(plot_y.get_ydata(), y[i]))
+				# plot_pred_y.set_xdata(np.append(plot_pred_y.get_xdata(), x))
+				# plot_pred_y.set_ydata(np.append(plot_pred_y.get_ydata(), y[i]))
+			self.last_x = x
+
+		plt.draw()
